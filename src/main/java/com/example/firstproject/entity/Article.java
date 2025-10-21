f30e38dc88863c8dd02db057d3ec07d704bb867e
Geunsuking <kimgs2024@gmail.com>
2025-10-21 17:37:15 +0900

    first project 2025 10/21

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,33 +4,29 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
+@Getter
 public class Article {
+
     @Id
     @GeneratedValue
     private Long id;
+
     @Column
     private String title;
+
     @Column
     private String content;
 
-    // Article 생성자
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
-    // tostring
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+
 }
 
