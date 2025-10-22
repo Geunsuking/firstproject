ffc2259b452c9ffd4a5363b3cf8881775e453b6b
Geunsuking <kimgs2024@gmail.com>
2025-10-22 22:23:50 +0900

    집에서 한거 2025-10-22

--- src/main/java/com/example/firstproject/sevice/ArticleService.java
+++ src/main/java/com/example/firstproject/sevice/ArticleService.java
@@ -3,11 +3,13 @@ package com.example.firstproject.sevice;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ArticleService {
     @Autowired
@@ -27,5 +29,35 @@ public class ArticleService {
             return null;
         }
         return articleRepository.save(article);
+    }
+
+    public Article update(Long id, ArticleForm dto) {
+        // 1. DTO -> 엔티티 변환하기
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        // 2. 타깃 조회하기
+        Article target = articleRepository.findById(id).orElse(null);
+        // 3. 잘못된 요청 처리하기
+        if (target == null || id != article.getId()) {
+            // 400, 잘못된 요청 응답!
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return null;
+        }
+        // 4. 업데이트 및 정상 응답(200)하기
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated;
+    }
+
+    public Article delete(Long id) {
+       // 1. 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+       //  2. 잘못된 요청 처리하기
+        if (target == null ){
+            return null;
+        }
+        // 3. 대상 삭제하기
+        articleRepository.delete(target);
+        return target;
     }
 }
